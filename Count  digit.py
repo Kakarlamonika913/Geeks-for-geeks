a403d321604acccef0862810f42ef6f8a2d7a4b1
Kakarlamonika913 <135260218+Kakarlamonika913@users.noreply.github.com>
2023-06-03 23:36:22 +0530

    Create Count  digit.py

---
+++ Count  digit.py
@@ -0,0 +1,57 @@
+#User function Template for python3
+
+
+
+
+
+class Solution:
+
+    def evenlyDivides (self, N):
+
+        # code here
+
+        temp=N
+
+        count=0
+
+        while(temp!=0):
+
+            x=temp%10
+
+            temp=temp//10
+
+            if x>0 and N%x==0:
+
+                count+=1
+
+        return count;
+
+
+
+
+
+#{ 
+
+ # Driver Code Starts
+
+#Initial Template for Python 3
+
+
+
+if __name__ == '__main__': 
+
+    t = int (input ())
+
+    for _ in range (t):
+
+        N = int(input())
+
+       
+
+
+
+        ob = Solution()
+
+        print(ob.evenlyDivides(N))
+
+# } Driver Code Ends
