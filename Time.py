780ad2d07bf91166ca24991d461ef0e2e13fc19f
Kakarlamonika913 <135260218+Kakarlamonika913@users.noreply.github.com>
2023-08-11 22:43:51 +0530

    Create Time.py

---
+++ Time.py
@@ -0,0 +1,45 @@
+#User function Template for python3
+
+
+
+class Solution:
+
+    def mulClock(self, num1, num2):
+
+        # code here
+
+        pdt = num1*num2     # Product of the Numbers
+
+        ans = pdt%12       # Remainder of the Product when divided by 12.
+
+        
+
+        return ans;
+
+
+
+
+
+#{ 
+
+ # Driver Code Starts
+
+#Initial Template for Python 3
+
+
+
+if __name__ == '__main__': 
+
+    t = int (input ())
+
+    for _ in range (t):
+
+        num1,num2=map(int,input().split())
+
+        
+
+        ob = Solution()
+
+        print(ob.mulClock(num1,num2))
+
+# } Driver Code Ends
