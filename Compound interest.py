c0b7e0d6a5c7dfdbc740ce8bcd4d88520554577e
Kakarlamonika913 <135260218+Kakarlamonika913@users.noreply.github.com>
2023-08-15 21:40:37 +0530

    Create Compound interest.py

---
+++ Compound interest.py
@@ -0,0 +1,51 @@
+#User function Template for python3
+
+
+
+class Solution:
+
+    def getCompundInterest(self, P ,T , N , R):
+
+        
+
+        amt = int(P * math.pow( (1 + (R / 100) / N ), N * T))
+
+        
+
+        return amt
+
+
+
+#This Code is Contributed by Sahil Rai.
+
+        
+
+
+
+
+
+#{ 
+
+ # Driver Code Starts
+
+#Initial Template for Python 3
+
+
+
+import math
+
+if __name__ == '__main__': 
+
+    t = int (input ())
+
+    for _ in range (t):
+
+        P,T,N,R = map(int,input().split())
+
+        
+
+        ob = Solution()
+
+        print(ob.getCompundInterest(P,T,N,R))
+
+# } Driver Code Ends
