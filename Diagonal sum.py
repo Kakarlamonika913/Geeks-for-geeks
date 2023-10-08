87a7484eadcfcc9714d9dcaf5be7fc5ae9b31ed7
Kakarlamonika913 <135260218+Kakarlamonika913@users.noreply.github.com>
2023-10-08 21:58:00 +0530

    Create Diagonal sum.py

---
+++ Diagonal sum.py
@@ -0,0 +1,25 @@
+#User function Template for python3
+
+class Solution:
+	def DiagonalSum(self, matrix):
+		# Code here
+		sum=0
+		for i in range(len(matrix)):
+		    sum+=matrix[i][i]+matrix[i][len(matrix)-1-i]
+        return sum
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__':
+	T=int(input())
+	for i in range(T):
+		n = int(input())
+		matrix = []
+		for _ in range(n):
+			matrix.append(list(map(int,input().split())))
+		ob = Solution()
+		ans = ob.DiagonalSum(matrix)
+		print(ans)
+# } Driver Code Ends
