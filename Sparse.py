8e86a9e5d42df4507bebda13d0cae0cced01aaf2
Kakarlamonika913 <135260218+Kakarlamonika913@users.noreply.github.com>
2023-11-04 22:20:08 +0530

    Create Sparse.py

---
+++ Sparse.py
@@ -0,0 +1,73 @@
+#User function Template for python3
+
+
+
+class Solution:
+
+    
+
+    #Function to check if the number is sparse or not.
+
+    def isSparse(self,n):
+
+        #Your code here 
+
+        return not n&(n<<1)
+
+
+
+
+
+
+
+#{ 
+
+ # Driver Code Starts
+
+#Initial Template for Python 3
+
+
+
+import math
+
+
+
+
+
+
+
+def main():
+
+    
+
+    T=int(input())
+
+    
+
+    while(T>0):
+
+        
+
+        
+
+        n=int(input())
+
+        ob=Solution()
+
+        if ob.isSparse(n):
+
+            print("1")
+
+        else:
+
+            print("0")
+
+        T-=1
+
+
+
+if __name__=="__main__":
+
+    main()
+
+# } Driver Code Ends
