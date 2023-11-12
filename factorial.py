9731d919e25dc122ab8c0cce8e2bf8febcbc15a1
Kakarlamonika913 <135260218+Kakarlamonika913@users.noreply.github.com>
2023-11-12 18:13:44 +0530

    Create factorial.py

---
+++ factorial.py
@@ -0,0 +1,9 @@
+lass Solution:
+    def factorial (self, N):
+        # code here
+        i=1
+        fact=1
+        while(i<=N):
+            fact=fact*i
+            i=i+1
+        return fact    
