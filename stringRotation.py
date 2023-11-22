b7940387ac31eb982ce0747dc2ae08c6faab56f9
Kakarlamonika913 <135260218+Kakarlamonika913@users.noreply.github.com>
2023-11-22 20:58:25 +0530

    Create stringRotation.py

---
+++ stringRotation.py
@@ -0,0 +1,14 @@
+#User function Template for python3
+
+class Solution:
+    
+    #Function to check if two strings are rotations of each other or not.
+    def areRotations(self,s1,s2):
+        #code here
+        if len(s1) != len(s2):
+           return False
+        s3 = s1 + s1
+        if s2 in s3:
+            return True
+        return False
+
