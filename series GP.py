2246bbf59aca891fc82bbc26b9151146ce17626d
Kakarlamonika913 <135260218+Kakarlamonika913@users.noreply.github.com>
2023-12-04 22:01:45 +0530

    Create series GP.py

---
+++ series GP.py
@@ -0,0 +1,9 @@
+res=1
+        n=n-1
+        while(n>0):
+            if(n%2!=0):
+                res=(res*r)%(pow(10,9)+7)
+            r=r*r%(pow(10,9)+7)
+            n=n//2
+        return (a*res)%(pow(10,9)+7)
+
