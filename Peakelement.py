2000586c6dce14bc3e8e11c712b713ab830be208
Kakarlamonika913 <135260218+Kakarlamonika913@users.noreply.github.com>
2024-01-22 19:44:05 +0530

    Create Peakelement.py

---
+++ Peakelement.py
@@ -0,0 +1,16 @@
+function should return index to the any valid peak element
+
+class Solution:   
+
+    def peakElement(self,arr, n):
+
+        # Code here
+
+        i=max(arr)
+
+        return arr.index(i)
+
+
+
+
+
